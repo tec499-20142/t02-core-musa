a79e6d4595cb31e369f15ccf8d79afa57e0500f6
Filipe Boaventura <fmbboaventura@gmail.com>
2014-10-20 17:15:41 -0300

    Ula: Overflow na adção e subtração

--- rtl/ula.v
+++ rtl/ula.v
@@ -1,27 +1,42 @@
 module ula(
 	input signed [31:0] inA,
 	input signed [31:0] inB,
-	//input op;
-	input [3:0] fx,
+	input [3:0] func,
 	output reg signed [31:0] result,
-	output reg overF,
+	output reg overflow,
 	output reg zero
 );
 
-always @ (inA or inB or fx) begin
+always @ (inA or inB or func) begin
 	
-	case(fx)
+	case(func)
 	
-	4'b0000: result = inA + inB;
-	4'b0001: result = inA - inB;
-	4'b0010: result = inA * inB;
-	4'b0011: result = inA / inB;
-	4'b0100: result = inA & inB;
-	4'b0101: result = inA | inB;
-	
+	4'b0000: begin
+				result = inA + inB;
+				overflow = (inA[31] && inB[31] && !result[31]) || (!inA[31] && !inB[31] && result[31]);
+				end
+	4'b0001: begin
+				result = inA - inB;
+				overflow = (inA[31] && !inB[31] && !result[31]) || (!inA[31] && inB[31] && result[31]);
+				end
+	4'b0010: begin
+				result = inA * inB;
+				end
+	4'b0011: begin
+				result = inA / inB;
+				if(inB == 0) overflow = 1;
+				end
+	4'b0100: begin
+				result = inA & inB;
+				overflow = 0;
+				end
+	4'b0101: begin
+				result = inA | inB;
+				overflow = 0;
+				end
 	endcase
 	
-	if(result == 8'b00000000) begin
+	if(result == 0) begin
 		zero = 1;
 	end else begin
 		zero = 0;
--- sim/ula_tb.sv
+++ sim/ula_tb.sv
@@ -1,25 +1,47 @@
 module ula_tb;
-  reg signed [31:0] a;
-	reg signed [31:0] b;
-	reg [3:0] f;
+  reg signed [31:0] op1;
+	reg signed [31:0] op2;
+	reg [3:0] op_code;
 	wire signed [31:0] res;
 	wire of;
 	wire z;
 	
+	// Variaveis de teste
+	integer expected_result;
+	integer op_count, ex_count;
+	
 	ula u(
-	 .inA(a),
-	 .inB(b),
-	 .fx(f),
+	 .inA(op1),
+	 .inB(op2),
+	 .func(op_code),
 	 .result(res),
-	 .overF(of),
+	 .overflow(of),
 	 .zero(z)
 	);
 	
 	initial begin
-	  $monitor("%d, %d", res, z);
-	  a = 1;
-	  b = 2;
-	  f = 1;
-	  #1;
+	  
+	  op_code = 1;
+	  op_count = 1;
+	  ex_count = 10;
+	  
+	  repeat(op_count) begin
+	    
+	    repeat(ex_count) begin
+	      op1 = $random;
+	      op2 = $random;
+	      #1;
+	      $display("Operando 1: %b\nOperando 2: %b\nResultado : %b\nOverflow: %b\n", op1, op2, res, of);
+
+	      if(((op1 + op2) > 2147483647) || ((op1 + op2) < -2147483648)) begin
+	      check_overflow : assert (of) 
+	         begin
+	             $display("Resultado: %d, Overflow: %b", res, of);
+	         end
+	      end
+	    end 
+	    
+	    op_code = op_code + 1;
+	  end // end repeat
 	end
 endmodule	
