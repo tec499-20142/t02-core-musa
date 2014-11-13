a37dce53b102fb82266574a2d5ed14d2ea99393a
Diego Leite <diegojleite@gmail.com>
2014-11-13 13:29:49 -0300

    [Refactoring] Remodelação da ULA

--- rtl/alu.v
+++ rtl/alu.v
@@ -1,48 +1,16 @@
 module alu(
-	input signed [31:0] inA,
-	input signed [31:0] inB,
-	input [3:0] func,
+	input signed [31:0] op1,
+	input signed [31:0] op2,
+	input [2:0] func,
 	output reg signed [31:0] result,
 	output reg overflow,
+	output reg equals,
+	output reg above,
 	output reg zero
 );
 
-always @ (inA or inB or func) begin
-	
-	case(func)
-	
-	4'b0000: begin
-				result = inA + inB;
-				overflow = (inA[31] && inB[31] && !result[31]) || (!inA[31] && !inB[31] && result[31]);
-				end
-	4'b0001: begin
-				result = inA - inB;
-				overflow = (inA[31] && !inB[31] && !result[31]) || (!inA[31] && inB[31] && result[31]);
-				end
-	4'b0010: begin
-				result = inA * inB;
-				end
-	4'b0011: begin
-				result = inA / inB;
-				if(inB == 0) overflow = 1;
-				end
-	4'b0100: begin
-				result = inA & inB;
-				overflow = 0;
-				end
-	4'b0101: begin
-				result = inA | inB;
-				overflow = 0;
-				end
-	endcase
-	
-	if(result == 0) begin
-		zero = 1;
-	end else begin
-		zero = 0;
-	end
+always @ (func) begin
 
 end
-	
 
 endmodule
