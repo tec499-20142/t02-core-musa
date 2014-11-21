0a0fcfde56086d092898bddab9533507d533d3e9
gordinh <lucas.ecomp2012@gmail.com>
2014-11-21 00:01:41 -0300

    Modificações no PC e Extensor + adição do somador

--- rtl/program_counter.v
+++ rtl/program_counter.v
@@ -1,11 +1,9 @@
 //Program Counter
 module program_counter(
-	input sinal_uc,
-	input reset,
 	input read_adress_in[17:0],
 	output read_adress_out[17:0]);
 	
-always @(posedge sinal_uc)
+always @(*)
 	begin
 	read_adress_out = read_adress_in
 	end
--- rtl/sign_extend.v
+++ rtl/sign_extend.v
@@ -1,12 +1,11 @@
 //Sign Extend
 module sign_extend(
-input[15:0] extend,
-input sinal_uc,
+input[15:0] extend
 output[31:0] extended);
 
 reg[31:0] extended;
 wire[15:0] extend;
-always @( posedge sinal_uc )
+always @(*)
 	begin
 		extended[31:0] <= { {16{extend[15]}}, extend[15:0] };
 	end
