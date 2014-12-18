324c37f7469e14f02f97a826315399138308f057
mtcastro <matheus.castro.fsa@gmail.com>
2014-12-18 11:46:39 -0300

    Musa compilando (Victor, Lucas e Matheus)

---
+++ rtl/mux_5.v
@@ -0,0 +1,17 @@
+module mux_5 (
+
+	input[5:0] in0 ,in1,
+	input ctrl,
+	
+	output reg [5:0] out
+);
+
+always @(*) begin
+	if(ctrl) begin
+		out <= in1;
+	end else begin 
+		out <= in0;
+	end
+end
+
+endmodule
